4d1d3fbef1a1cfb7d359457a00ba70a8ffbfc0b9
André Pedruzzi <andrerpedruzzi@gmail.com>
2019-08-14 20:14:24 -0300

    remove cors

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 
 // Fornece acesso aos arquivos da pasta build que contem a aplicação React compilada
 app.use(express.static(__dirname + '/client/build/'));
