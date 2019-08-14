85cdd9291319faef7fc8fa0f674be74840d66bc6
André Pedruzzi <andrerpedruzzi@gmail.com>
2019-08-14 18:38:20 -0300

    v2

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import request from 'request';
+
+function App() {
+  const [resposta, setResposta] = useState({});
+  useEffect(() => {
+    request( 'http://localhost:'+ process.env.PORT || 5000 +'/api/mensagem', function(err, res, body){
+      console.log('resposta: ', JSON.parse(body) );
+      setResposta(JSON.parse(body));
+    })
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+          {resposta.message}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
